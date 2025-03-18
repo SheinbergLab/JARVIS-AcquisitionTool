637616c009d67b011edc804d418f9408787a4ce6
David Sheinberg <David_Sheinberg@brown.edu>
2025-03-17 23:21:16 -0400

    added message tcp/ip server and message handler

--- main.cpp
+++ main.cpp
@@ -13,15 +13,39 @@
 #include "globals.hpp"
 #include "mainwindow.hpp"
 #include "triggerinterface.hpp"
+#include "tcpipserver.hpp"
 
 #include <QApplication>
 #include <QImage>
 #include <QSettings>
 #include <QSplashScreen>
+#include <QDebug>
 #include <iostream>
+
+#include "tcpipserver.hpp"
 
 Q_DECLARE_METATYPE(QList<int>)
 Q_DECLARE_METATYPE(statusType)
+
+// called when a TCP/IP message is received
+void handleMessage(TcpipServer *server,
+		   MainWindow *m_window,
+		   const QString &clientAddress,
+		   const QByteArray &message,
+		   const int clientId) {
+
+  qDebug() << "Processing message from" << clientAddress << "ID:" << clientId;
+
+  // test by toggle main tab
+  static int tab_id = 0;
+  tab_id = !tab_id;
+  m_window->setMode(tab_id);
+
+  QByteArray response = "Processed: " + message.toUpper();
+  
+  server->sendResponse(clientId, response);    
+}
+
 
 int main(int argc, char **argv) {
     qSetMessagePattern(QStringLiteral("%{time}"
@@ -59,7 +83,20 @@ int main(int argc, char **argv) {
     // delayl(100);
     FlirConfigBackend::getInstance();
 
+    TcpipServer server;
+    const int port = 1234;
+
     MainWindow m_window;
+
+    // Connect the signal to a handler using lambda to include server
+    QObject::connect(&server, &TcpipServer::messageReceived,
+		     [&server, &m_window](const QString &clientAddress,
+					 const QByteArray &message,
+					 int clientId) {
+		       handleMessage(&server, &m_window,
+				     clientAddress, message, clientId);});
+    server.start(port);
+
     m_window.show();
     // splash.finish(&m_window);
     return app.exec();
